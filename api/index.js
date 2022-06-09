56ee0306b9789f73a29b94550a15ef5c7263d9a6
kenjius01 <63000038+kenjius01@users.noreply.github.com>
2022-06-10 00:02:50 +0700

    feat: api hotel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoute from './routes/auth.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+import usersRoute from './routes/users.js'
+
 
 const app = express();
 dotenv.config();
@@ -18,9 +23,19 @@ mongoose.connection.on("disconnected", () => {
   console.log('MongoDB disconnected!');
 });
 
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB connected!');
-});
+
+//Middlewares
+
+app.use(express.json())
+
+app.use('/api/auth', authRoute)
+app.use('/api/users', usersRoute)
+app.use('/api/hotels', hotelsRoute)
+app.use('/api/rooms', roomsRoute)
+
+app.use((err, req, res, next) => {
+  return res.status(500).json('Error from handle!')
+})
 
 app.listen(8800, () => {
   connect();
