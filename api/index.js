36976f5ce76d423cf66151382b694625606b0d7c
kenjius01 <63000038+kenjius01@users.noreply.github.com>
2022-06-17 00:15:26 +0700

    feat: add auth api

--- api/index.js
+++ api/index.js
@@ -1,43 +1,50 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import authRoute from './routes/auth.js'
-import hotelsRoute from './routes/hotels.js'
-import roomsRoute from './routes/rooms.js'
-import usersRoute from './routes/users.js'
+import authRoute from './routes/auth.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+import usersRoute from './routes/users.js';
 
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
 
 const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log('Connect to mongodb');
-  } catch (error) {
-    throw error;
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Connect to mongodb');
+    } catch (error) {
+        throw error;
+    }
 };
 
-mongoose.connection.on("disconnected", () => {
-  console.log('MongoDB disconnected!');
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected!');
 });
 
-
 //Middlewares
+app.use(cookieParser())
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/auth', authRoute)
-app.use('/api/users', usersRoute)
-app.use('/api/hotels', hotelsRoute)
-app.use('/api/rooms', roomsRoute)
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
 
 app.use((err, req, res, next) => {
-  return res.status(500).json('Error from handle!')
-})
-
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 app.listen(8800, () => {
-  connect();
-  console.log('Connect to backend');
+    connect();
+    console.log('Connect to backend');
 });
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get('/', (req, res)=> {
-    res.send("hello, this is auth page")
-})
+// REGISTER
+router.post('/register', register)
+
+// LOGIN
+router.post('/login', login)
 
 export default router
